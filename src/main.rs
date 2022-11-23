03fc2cba2407738ca2b25a1bb04e6b2edcbdf1d7
Tomy Falgui <tomyfalgui@gmail.com>
2022-11-23 23:40:34 +0800

    print out inner text of .wiki-page-content

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use reqwest;
+use tl;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -15,17 +16,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let parsed_result = fetch_subreddit_wiki(&args.subreddit)?;
 
-    println!("{}", parsed_result);
+    let r_formatted = format!(r"{}", parsed_result);
+    let dom = tl::parse(&r_formatted, tl::ParserOptions::default()).unwrap();
+    let parser = dom.parser();
+    dom.query_selector(".wiki-page-content")
+        .expect("Failed to find container")
+        .for_each(move |el| {
+            let element = el.get(parser).unwrap();
+            println!("{}", element.inner_text(parser).into_owned());
+        });
 
     Ok(())
 }
 
 fn fetch_subreddit_wiki(subreddit: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get(format!(
-        "https://old.reddit.com/r/{}/wiki/index",
-        subreddit
-    ))?
-    .text()?;
+    let resp =
+        reqwest::blocking::get(format!("https://old.reddit.com/r/{}/wiki/index", subreddit))?
+            .text()?;
 
     Ok(resp)
 }
